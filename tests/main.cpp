706f137624fad3aedc9056d3edead32af4e86ed6
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2020-07-10 09:19:57 +0200

    Added tests folder, its empty for the time being. Gitignore updated

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "../src/Settings.h"
+#include "../src/window/Window.h"
+#include "../src/window/MainWindow.h"
+
+
+
+
+TEST_CASE("Create Basic Main Window and check its functionality") {
+}
